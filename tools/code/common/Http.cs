ee7fe074a22bfa9c29bf2ead60fbba4446673fa7
guythetechie <31325620+guythetechie@users.noreply.github.com>
2022-11-15 18:29:51 -0500

    Fix for long-running operations (#148)
    * Waits for long-running operations to complete
    
    * Fix serialization error in API diagnostic

--- tools/code/common/Http.cs
+++ tools/code/common/Http.cs
@@ -4,6 +4,7 @@ using Azure.Core.Pipeline;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -48,7 +49,7 @@ public static class HttpPipelineExtensions
         var request = pipeline.CreateRequest(uri, RequestMethod.Get);
 
         var response = await pipeline.SendRequestAsync(request, cancellationToken);
-        response.Validate();
+        response.Validate(uri);
 
         return response.Content;
     }
@@ -57,7 +58,7 @@ public static class HttpPipelineExtensions
     {
         var request = pipeline.CreateRequest(uri, RequestMethod.Delete);
         var response = await pipeline.SendRequestAsync(request, cancellationToken);
-        response.Validate();
+        response.Validate(uri);
     }
 
     public static async ValueTask PutResource(this HttpPipeline pipeline, Uri uri, JsonObject resource, CancellationToken cancellationToken)
@@ -68,7 +69,8 @@ public static class HttpPipelineExtensions
         request.Headers.Add("Content-type", "application/json");
 
         var response = await pipeline.SendRequestAsync(request, cancellationToken);
-        response.Validate();
+        response.Validate(uri);
+        await pipeline.WaitForLongRunningOperation(response, cancellationToken);
     }
 
     private static Request CreateRequest(this HttpPipeline pipeline, Uri uri, RequestMethod requestMethod)
@@ -80,10 +82,34 @@ public static class HttpPipelineExtensions
         return request;
     }
 
-    private static Response Validate(this Response response)
+    private static Response Validate(this Response response, Uri requestUri)
     {
         return response.IsError
-            ? throw new InvalidOperationException($"HTTP request to URI failed with status code {response.Status}. Content is '{response.Content}'.")
+            ? throw new InvalidOperationException($"HTTP request to URI {requestUri} failed with status code {response.Status}. Content is '{response.Content}'.")
             : response;
+    }
+
+    private static async ValueTask WaitForLongRunningOperation(this HttpPipeline pipeline, Response response, CancellationToken cancellationToken)
+    {
+        var updatedResponse = response;
+        while ((updatedResponse.Status == ((int)HttpStatusCode.Accepted))
+               && updatedResponse.Headers.TryGetValue("Location", out var locationHeaderValue)
+               && Uri.TryCreate(locationHeaderValue, UriKind.Absolute, out var locationUri)
+               && locationUri is not null)
+        {
+            if (updatedResponse.Headers.TryGetValue("Retry-After", out var retryAfterString) && int.TryParse(retryAfterString, out var retryAfterSeconds))
+            {
+                var retryAfterDuration = TimeSpan.FromSeconds(retryAfterSeconds);
+                await Task.Delay(retryAfterDuration, cancellationToken);
+            }
+            else
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+
+            var request = pipeline.CreateRequest(locationUri, RequestMethod.Get);
+            updatedResponse = await pipeline.SendRequestAsync(request, cancellationToken);
+            updatedResponse.Validate(locationUri);
+        }
     }
 }
