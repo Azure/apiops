68776ececc326d8bb4cc6573b5db2f33c5856460
guythetechie <31325620+guythetechie@users.noreply.github.com>
2022-12-02 16:19:57 -0500

    Fix diagnostics serialization

--- tools/code/common/Diagnostic.cs
+++ tools/code/common/Diagnostic.cs
@@ -232,7 +232,7 @@ public sealed record DiagnosticModel
                         new JsonObject()
                             .AddPropertyIfNotNull("headers", Headers?.Select(x => x.Serialize())
                                                                     ?.ToJsonArray())
-                            .AddPropertyIfNotNull("queryParams", Headers?.Select(x => x.Serialize())
+                            .AddPropertyIfNotNull("queryParams", QueryParams?.Select(x => x.Serialize())
                                                                         ?.ToJsonArray());
 
                     public static DataMaskingSettings Deserialize(JsonObject jsonObject) =>
