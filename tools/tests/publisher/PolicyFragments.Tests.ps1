42e114414c17e75910edd81acf43d236c00af197
guythetechie <31325620+guythetechie@users.noreply.github.com>
2023-02-23 13:09:47 -0500

    Updates

---
+++ tools/tests/publisher/PolicyFragments.Tests.ps1
@@ -0,0 +1,44 @@
+param (
+    [Parameter(Mandatory)]
+    [string]
+    $PublisherArtifactsPath,
+    [Parameter(Mandatory)]
+    [string]
+    $ExtractorArtifactsPath
+)
+
+BeforeAll {
+    Set-StrictMode -Version Latest
+    $ErrorActionPreference = "Stop"
+    $VerbosePreference = "Continue"
+    $InformationPreference = "Continue"
+}
+
+BeforeDiscovery {
+    $policyFragmentsPathSection = "policy fragments"
+    $parameters = Join-Path "$PublisherArtifactsPath" "$policyFragmentsPathSection" |
+                    Get-ChildItem -Directory |
+                    ForEach-Object {
+                        @{
+                            name                       = $_.Name
+                            policyFragmentsPathSection = $policyFragmentsPathSection
+                        }
+                    }
+}
+
+Describe "policy fragment <_.name>" -ForEach $parameters {
+    BeforeDiscovery {
+    }
+
+    It "Publishes XML successfully" {
+        $xmlPathSection = Join-Path "$policyFragmentsPathSection" "$name" "policy.xml"
+
+        $publisherXmlPath = Join-Path "$PublisherArtifactsPath" "$xmlPathSection"
+        $publisherXmlContent = Get-Content -Path "$publisherXmlPath" -Raw
+
+        $extractorXmlPath = Join-Path "$ExtractorArtifactsPath" "$xmlPathSection"
+        $extractorXmlContent = Get-Content -Path "$extractorXmlPath" -Raw
+
+        $extractorXmlContent | Should -Be $publisherXmlContent
+    }
+}
