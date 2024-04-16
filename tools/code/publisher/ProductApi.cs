c1ba8a0c903b4064c6df2c667997e89b22efa13b
Jeroen Maes <jeroen.maes@cegeka.com>
2024-04-16 07:56:28 +0200

    Add retry

--- tools/code/publisher/ProductApi.cs
+++ tools/code/publisher/ProductApi.cs
@@ -181,9 +181,19 @@ internal static class ProductApi
             }
             catch (HttpRequestException httpRequestException) when (httpRequestException.Message.Contains("API not found"))
             {
-                logger.LogWarning("API not found, the API will not be added to the Product.");
-
-                return;
+                retryCount++;
+                if (retryCount <= 3)
+                {
+                    // Log the retry attempt
+                    logger.LogWarning("Retrying API put operation for {apiName}. Retry attempt: {retryCount}", apiName, retryCount);
+                    // Wait for a certain duration before retrying
+                    await Task.Delay(TimeSpan.FromSeconds(Math.Pow(2, retryCount)), cancellationToken);
+                }
+                else
+                {
+                    // Retry limit reached, log as warning
+                    logger.LogWarning("API {apiName} not found, the API will not be added to product in the target environment.", apiName);
+                }
             }
         }
     }
