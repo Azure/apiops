65ca90d1b3723d2723cb9e17969677583acb181d
James Woodbridge <james@bloogsy.net>
2024-10-12 00:56:22 +0100

    Return an empty list if groups are not supported in pricing tier (#687)
    Co-authored-by: James Woodbridge <james.woodbridge@tpr.gov.uk>

--- tools/code/common/Group.cs
+++ tools/code/common/Group.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
@@ -149,10 +150,19 @@ public static class GroupModule
                                                                 .Delete(pipeline, cancellationToken),
                                cancellationToken);
 
-    public static IAsyncEnumerable<GroupName> ListNames(this GroupsUri uri, HttpPipeline pipeline, CancellationToken cancellationToken) =>
-        pipeline.ListJsonObjects(uri.ToUri(), cancellationToken)
+    public static IAsyncEnumerable<GroupName> ListNames(this GroupsUri uri, HttpPipeline pipeline, CancellationToken cancellationToken)
+    {
+        var exceptionHandler = (HttpRequestException exception) =>
+            exception.StatusCode == HttpStatusCode.BadRequest
+             && exception.Message.Contains("MethodNotAllowedInPricingTier", StringComparison.OrdinalIgnoreCase)
+            ? AsyncEnumerable.Empty<GroupName>()
+            : throw exception;
+
+        return pipeline.ListJsonObjects(uri.ToUri(), cancellationToken)
                 .Select(jsonObject => jsonObject.GetStringProperty("name"))
-                .Select(GroupName.From);
+                .Select(GroupName.From)
+                .Catch(exceptionHandler);
+    }
 
     public static IAsyncEnumerable<(GroupName Name, GroupDto Dto)> List(this GroupsUri groupsUri, HttpPipeline pipeline, CancellationToken cancellationToken) =>
         groupsUri.ListNames(pipeline, cancellationToken)
