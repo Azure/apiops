6e9f9b73d1a76c1811e6e578aced64615b17591a
guythetechie <31325620+guythetechie@users.noreply.github.com>
2024-11-07 12:55:44 -0500

    Merge pull request #702 from Azure/devcontainer
    Update dev container

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Aspire
-sudo /usr/share/dotnet/dotnet workload update --from-rollback-file "./.devcontainer/rollback.txt"
-sudo /usr/share/dotnet/dotnet workload install aspire --from-rollback-file "./.devcontainer/rollback.txt"
-
-# Install PowerShell modules
-pwsh -Command "Install-Module -Name Az -Force
-               Install-Module -Name Microsoft.Graph -Force"
