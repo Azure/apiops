7b8450bc796b1f2abd613ceae663a57dd9e04c8c
Padure Sergio <70955746+sergiopadure@users.noreply.github.com>
2025-08-21 16:45:03 +0200

    Update New-ExtractorConfiguration.ps1

--- tools/scripts/New-ExtractorConfiguration.ps1
+++ tools/scripts/New-ExtractorConfiguration.ps1
@@ -10,11 +10,11 @@ It requires at least one of SubscriptionId or SubscriptionName, along with ApimI
 .PARAMETER Environment
 The environment to use for the extractor configuration file. The default value is "AzureUSGovernment". Valid values are "AzureUSGovernment" and "AzureCloud"
 
+.PARAMETER TenantId
+The tenant ID to use for the extractor configuration file. This is the ID of the tenant in the Azure portal. Required because otherwise the latest version of Azure Powershell starts trying to connect to all the tenants available to the user then bugs out.
+
 .PARAMETER SubscriptionId
 The subscription ID to use for the extractor configuration file. This is the ID of the subscription in the Azure portal. This parameter is required if the SubscriptionName parameter is not specified.
-
-.PARAMETER SubscriptionName
-The subscription name to use for the extractor configuration file. This is the name of the subscription in the Azure portal. This parameter is required if the SubscriptionId parameter is not specified.
 
 .PARAMETER ApimInstanceName
 The name of the APIM instance to use for the extractor configuration file. This is the name of the APIM instance in the Azure portal.
@@ -26,7 +26,7 @@ The name of the resource group that contains the APIM instance to use for the ex
 The name of the stage to use for the extractor configuration file. This is the name of the stage in the Azure portal.
 
 .EXAMPLE
-. .\New-ExtractorConfiguration.ps1 -ApimInstanceName 'apim-instance' -ResourceGroupName 'rg-of-apiminstance' -Stage 'dev' -SubscriptionName 'subscription-of-apim'
+. .\New-ExtractorConfiguration.ps1 -TenantId 'tenant-id' -ApimInstanceName 'apim-instance' -ResourceGroupName 'rg-of-apiminstance' -Stage 'dev' -SubscriptionId 'subscriptionid-of-apim'
 
 .NOTES
 Ensure you have the necessary Azure permissions to execute the script.
@@ -36,15 +36,15 @@ Ensure you have the necessary Azure permissions to execute the script.
 param(
     [string]
     [ValidateSet("AzureUSGovernment","AzureCloud")]
-    $Environment = "AzureUsGovernment",
+    $Environment = "AzureCloud",
 
     [string]
-    [Parameter(Mandatory, ParameterSetName = "SubscriptionId")]
+    [Parameter(Mandatory)]
+    $TenantId = '',
+
+    [string]
+    [Parameter(Mandatory)]
     $SubscriptionId = '',
-
-    [string]
-    [Parameter(Mandatory, ParameterSetName = "SubscriptionName")]
-    $SubscriptionName = '',
 
     [string]
     [Parameter(Mandatory)]
@@ -63,14 +63,7 @@ param(
 $context = Get-AzContext
 if ($null -eq $Context)
 {
-    Login-AzAccount -Environment $Environment
-}
-if ($SubscriptionId.Length -gt 0) {
-    Set-AzContext -SubscriptionId $SubscriptionId
-} 
-else
-{
-    Set-AzContext -SubscriptionName $SubscriptionName
+    Connect-AzAccount -Tenant $TenantId -Environment $Environment -Subscription $SubscriptionId
 }
 
 # Encapsulate potential error throwing operations within a try-catch block
@@ -82,15 +75,21 @@ try {
     $products = Get-AzApiManagementProduct -Context $apim
     $backends = Get-AzApiManagementBackend -Context $apim
 
-    $uri = $Environment -eq "AzureUsGovernment" ? "management.usgovcloudapi.net" : "management.azure.com"
-    $headers = @{
-        "Authorization" = "Bearer $((Get-AzAccessToken -ResourceUrl "https://$uri").Token)"
+    $uri = switch ($Environment) {
+        "AzureUsGovernment" { "https://management.usgovcloudapi.net" }
+        "AzureCloud" { "https://management.azure.com" }
     }
-    
-    $tags = (Invoke-RestMethod -Method GET -Uri "https://$uri/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/tags?api-version=2022-08-01" -Headers $headers).value.name
-    
+
+    $token = (ConvertFrom-SecureString (Get-AzAccessToken -ResourceUrl $uri).Token -AsPlainText)
+    $headers = @{
+        "Authorization" = "Bearer $token"
+    }
+    $TagsUri = "$uri/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/tags?api-version=2022-08-01"
+    $TagsUri
+    $tags = (Invoke-RestMethod -Method GET -Uri $TagsUri -Headers $headers).value.name
+
 } catch {
-    Write-Error "Error: $_"
+    Write-Error "$_"
     exit 1
 }
 #endregion Azure Queries
@@ -152,4 +151,5 @@ $fileContent = $StringBuilder.ToString()
 Out-File -FilePath "autogen.configuration.extractor.$Stage.yaml" -InputObject $fileContent -Force
 
 # Output completion message
+Write-Output "Done listing APIs, tags, loggers, namedValues, products, and backends"
 Write-Output "Done listing APIs, tags, loggers, namedValues, products, and backends"
